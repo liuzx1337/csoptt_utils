33d9b196ceae9cffdd12a0811a0faa2668ff1591
刘子西 <Liuzx1337@163.com>
2018-09-07 14:00:50 +0800

    redis配置：增加默认值

--- src/main/java/com/csoptt/utils/jedis/RedisConfig.java
+++ src/main/java/com/csoptt/utils/jedis/RedisConfig.java
@@ -4,6 +4,12 @@ package com.csoptt.utils.jedis;
  * Redis配置
  *
  * <p>通常可以通过注入等方式，为此类的各个静态变量赋值，并且加载到Redis连接池中</p>
+ *
+ * <b>redisHost、redisPort 必配，否则无法连接</b>
+ * <b>redisPassword 可配</b>
+ * <b>redisDbnum 可配，默认0</b>
+ * <b>maxIdle、maxTotal、maxWait、testOfBorrow、timeout 可配，均有默认值</b>
+ *
  * @see JedisUtil
  *
  * @author qishao
@@ -49,7 +55,7 @@ public class RedisConfig {
     /**
      * redis是否进行有效性检查
      */
-    public static boolean testOnBorrow;
+    public static boolean testOnBorrow = true;
 
     /**
      * redis连接池中连接超时时间
